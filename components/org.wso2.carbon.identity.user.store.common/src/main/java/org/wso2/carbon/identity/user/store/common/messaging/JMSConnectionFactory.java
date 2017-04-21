145df0675ee760a409264819964beb8ba2145591
godwin <godwinamila@gmail.com>
2017-04-21 18:25:31 +0530

    Move JMS related classes

--- components/org.wso2.carbon.identity.user.store.common/src/main/java/org/wso2/carbon/identity/user/store/common/messaging/JMSConnectionFactory.java
+++ components/org.wso2.carbon.identity.user.store.common/src/main/java/org/wso2/carbon/identity/user/store/common/messaging/JMSConnectionFactory.java
@@ -15,7 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.wso2.carbon.identity.user.store.outbound.messaging;
+package org.wso2.carbon.identity.user.store.common.messaging;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.commons.logging.Log;
